3fa6ab579286a0ffd577205b23cc06420727893e
Jeremy Shaw <jeremy@n-heptane.com>
2015-04-07 19:58:01 -0500

    added default.nix and shell.nix

---
+++ default.nix
@@ -0,0 +1,15 @@
+{ mkDerivation, base, bytestring, happstack-server, harp, hsp
+, hsx2hs, mtl, stdenv, syb, text, utf8-string
+}:
+mkDerivation {
+  pname = "happstack-hsp";
+  version = "7.3.4";
+  src = ./.;
+  buildDepends = [
+    base bytestring happstack-server harp hsp hsx2hs mtl syb text
+    utf8-string
+  ];
+  homepage = "http://www.happstack.com/";
+  description = "Support for using HSP templates in Happstack";
+  license = stdenv.lib.licenses.bsd3;
+}
